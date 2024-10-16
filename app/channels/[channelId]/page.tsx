d047aeff310d9b3c12306d67b664e91c8429baf5
Jeewhan <distinct19@gmail.com>
2024-10-16 20:47:49 +0900

    ✨ CanvasContainer 바깥에서 주입한 board를 바탕으로 render

--- app/channels/[channelId]/page.tsx
+++ app/channels/[channelId]/page.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { useEffect, useRef } from "react";
+
 import CanvasContainer from "@/src/shared/components/canvas-container";
+import { PuzzleBoard } from "@/src/entities/puzzle-board";
+import { ImageBlock } from "@/src/entities/block";
 
 const ChannelPage = ({ params }: { params: { channelId: string } }) => {
+  const boardRef = useRef<PuzzleBoard>(new PuzzleBoard());
+
+  useEffect(() => {
+    boardRef.current.subscribe(
+      new ImageBlock(
+        0,
+        0,
+        0,
+        0,
+        "https://image.aladin.co.kr/product/5592/81/cover500/3581198452_1.jpg"
+      )
+    );
+  }, []);
+
   return (
     <section className="flex w-full h-screen">
-      <CanvasContainer />
+      <CanvasContainer board={boardRef.current} />
     </section>
   );
 };
