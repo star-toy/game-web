d1a374aaabe6c82a36e0a6a3766c48e1a982bb59
Jeewhan <distinct19@gmail.com>
2024-10-16 16:26:49 +0900

    ♻️ useCanvas -> CanvasManager, canvas 관련 로직에서 React 관련 종속성 제거

--- src/shared/components/canvas-container.tsx
+++ src/shared/components/canvas-container.tsx
@@ -1,15 +1,29 @@
 "use client";
 
-import { useRef } from "react";
+import { RefObject, useEffect, useRef } from "react";
 
-import { useCanvas } from "../hooks/use-canvas";
 import { useClientWidthHeight } from "../hooks/use-client-width-height";
+
+import CanvasService from "@/src/entities/canvas-service";
 
 const CanvasContainer = () => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const canvasRef: RefObject<HTMLCanvasElement> =
+    useRef<HTMLCanvasElement>(null);
+  const canvasServiceRef = useRef<null | CanvasService>(null);
 
   const { width, height } = useClientWidthHeight(containerRef);
-  const { canvasRef } = useCanvas(width, height);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      canvasServiceRef.current = new CanvasService(canvasRef.current);
+      canvasServiceRef.current.resize(width, height);
+    }
+  }, [canvasRef.current]);
+
+  useEffect(() => {
+    canvasServiceRef.current?.resize(width, height);
+  }, [width, height]);
 
   return (
     <div ref={containerRef} className="w-full h-full">
