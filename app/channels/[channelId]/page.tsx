4b2b3c06362ce4992b3fa2808bdf1b0ed9f6f4ac
Jeewhan <distinct19@gmail.com>
2024-10-20 00:07:18 +0900

    🤡 MOCK_IMAGE_URL

--- app/channels/[channelId]/page.tsx
+++ app/channels/[channelId]/page.tsx
@@ -6,19 +6,14 @@ import CanvasContainer from "@/src/shared/components/canvas-container";
 import PuzzleBoard from "@/src/entities/puzzle/puzzle-board";
 import { ImageBlock } from "@/src/entities/puzzle/puzzle-block";
 
+const MOCK_IMAGE_URL =
+  "https://image.aladin.co.kr/product/5592/81/cover500/3581198452_1.jpg";
+
 const ChannelPage = ({ params }: { params: { channelId: string } }) => {
   const boardRef = useRef<PuzzleBoard>(new PuzzleBoard());
 
   useEffect(() => {
-    boardRef.current.subscribe(
-      new ImageBlock(
-        0,
-        0,
-        0,
-        0,
-        "https://image.aladin.co.kr/product/5592/81/cover500/3581198452_1.jpg"
-      )
-    );
+    boardRef.current.subscribe(new ImageBlock(0, 0, 0, 0, MOCK_IMAGE_URL));
   }, []);
 
   return (
