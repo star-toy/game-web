673d0073b8294f76a42e40ac8129a24a9a62e98f
Jeewhan <distinct19@gmail.com>
2024-10-21 15:10:14 +0900

    💄 canvas 크기 조정

--- app/channels/[channelId]/page.tsx
+++ app/channels/[channelId]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 import { useClientWidthHeight } from "@/src/shared/hooks/use-client-width-height";
 
@@ -10,11 +10,33 @@ const ChannelPage = ({
   params: { channelId: string };
 }) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
   const { width, height } = useClientWidthHeight(containerRef);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+
+      if (ctx) {
+        const devicePixelRatio = window.devicePixelRatio ?? 1;
+
+        canvas.style.width = `${width}px`;
+        canvas.style.height = `${height}px`;
+
+        canvas.width = Math.floor(width * devicePixelRatio);
+        canvas.height = Math.floor(height * devicePixelRatio);
+
+        ctx.scale(devicePixelRatio, devicePixelRatio);
+      }
+    }
+  }, [width, height]);
 
   return (
     <section ref={containerRef} className="flex w-full h-screen">
-      <canvas />
+      <canvas ref={canvasRef} />
     </section>
   );
 };
